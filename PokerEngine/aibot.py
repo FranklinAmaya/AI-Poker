c803293f76a9f4fb96f8ff898b914f16901468d3
bwz3kt <bwz3kt@virginia.edu>
2018-11-29 20:44:15 -0500

    Basic bot working

--- PokerEngine/aibot.py
+++ PokerEngine/aibot.py
@@ -1,16 +1,59 @@
 from pypokerengine.players import BasePokerPlayer
+from pypokerengine.engine.hand_evaluator import HandEvaluator
+from pypokerengine.utils.card_utils import _pick_unused_card, _fill_community_card, gen_cards, estimate_hole_card_win_rate
+
 
 class AIPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BasePokerPlayer"
 
-    #  we define the logic to make an action through this method. (so this method would be the core of your AI)
     def declare_action(self, valid_actions, hole_card, round_state):
-        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
+        
+        # get evaluation score of hand
+        print("================================================")
+        print("Cards: " + str(hole_card))
+        hand_score = HandEvaluator.eval_hand(gen_cards(hole_card), gen_cards(round_state['community_card']))
+        print("Hand score: " + str(hand_score))
+        win_rate = estimate_hole_card_win_rate(nb_simulation=1000, nb_player=2, hole_card=gen_cards(hole_card), community_card=gen_cards(round_state['community_card']))
+        print("Win rate: " + str(win_rate))
+
+        # Check whether it is possible to call
+        can_call = len([item for item in valid_actions if item['action'] == 'call']) > 0
+        if can_call:
+            # If so, compute the amount that needs to be called
+            call_amount = [item for item in valid_actions if item['action'] == 'call'][0]['amount']
+        else:
+            call_amount = 0
+
+        amount = None
+        
+        # If the win rate is large enough, then raise
+        if win_rate > 0.5:
+            raise_amount_options = [item for item in valid_actions if item['action'] == 'raise'][0]['amount']
+            if win_rate > 0.85:
+                # If it is extremely likely to win, then raise as much as possible
+                action = 'raise'
+                amount = raise_amount_options['max']
+            elif win_rate > 0.75:
+                # If it is likely to win, then raise by the minimum amount possible
+                action = 'raise'
+                amount = raise_amount_options['min']
+            else:
+                # If there is a chance to win, then call
+                action = 'call'
+        else:
+            action = 'call' if can_call and call_amount == 0 else 'fold'
+
+        # Set the amount
+        if amount is None:
+            items = [item for item in valid_actions if item['action'] == action]
+            amount = items[0]['amount']
+
+        '''
         call_action_info = valid_actions[1]
-        action, amount = call_action_info["action"], call_action_info["amount"]
-        return action, amount   # action returned here is sent to the poker engine
+        action, amount = call_action_info["action"], call_action_info["amount"] '''
+        return action, amount  # action returned here is sent to the poker engine
 
     def receive_game_start_message(self, game_info):
-        pass
+        self.n_players = game_info['player_num']
 
     def receive_round_start_message(self, round_count, hole_card, seats):
         pass
@@ -22,6 +65,11 @@ class AIPlayer(BasePokerPlayer):  # Do not forget to make parent class as "BaseP
         pass
 
     def receive_round_result_message(self, winners, hand_info, round_state):
+        '''
+        is_winner = self.uuid in [item['uuid'] for item in winners]
+        self.wins += int(is_winner)
+        self.losses += int(not is_winner)
+        '''
         pass
 
 
